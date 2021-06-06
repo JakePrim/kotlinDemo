576c2c516680870647c4acab6f97d367c32179c1
sufulu <sufulu@pdnews.cn>
2021-06-06 23:16:19 +0800

    nt

---
+++ Kotlin/lesson/src/main/java/com/example/lesson/LessonPresenter.kt
@@ -0,0 +1,39 @@
+package com.example.lesson
+
+import com.example.core.http.EntityCallback
+import com.example.core.http.HttpClient
+import com.example.core.utils.Utils.toast
+import com.example.lesson.entity.Lesson
+import com.google.gson.reflect.TypeToken
+import java.util.*
+//internal 修饰的类 只能被当前模块使用 其他模块无法使用
+class LessonPresenter(private val activity: LessonActivity) {
+    private var lessons: List<Lesson> = ArrayList()
+    private val type = object : TypeToken<List<Lesson?>?>() {}.type
+    fun fetchData() {
+        HttpClient[LESSON_PATH, type, object : EntityCallback<List<Lesson>> {
+            override fun onSuccess(entity: List<Lesson>) {
+                this@LessonPresenter.lessons = entity
+                activity.runOnUiThread { activity.showResult(entity) }
+            }
+
+            override fun onFailure(message: String?) {
+                activity.runOnUiThread { toast(message) }
+            }
+        }]
+    }
+
+    fun showPlayback() {
+        val playbackLessons: MutableList<Lesson> = ArrayList()
+        for (lesson in lessons) {
+            if (lesson.state === Lesson.State.PLAYBACK) {
+                playbackLessons.add(lesson)
+            }
+        }
+        activity.showResult(playbackLessons)
+    }
+
+    companion object {
+        private const val LESSON_PATH = "lessons"
+    }
+}
